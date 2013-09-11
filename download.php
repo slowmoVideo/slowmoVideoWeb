5f04889743dd9c9d1d0b62b7b5b4c9d54d59a342
Simon A. Eugster <simon.eu@gmail.com>
2013-09-11 10:05:37 +0200

    Update for single CMake file

--- download.php
+++ download.php
@@ -167,27 +167,17 @@ $ git clone git://github.com/slowmoVideo/slowmoVideo.git
 $ cd slowmoVideo
 $ git pull # ''Run this whenever you want to update to the latest source code version''
 $$
-Now slomoVideo needs to be compiled and installed.
-$$((title=slowmoVideo compiling))
-$ cd slowmoVideo # '''This is not the project’s root directory but a subdirectory!'''
+Now slomoVideo needs to be compiled and installed. (Note: This has changed recently, and flowBuilder does not need to be compiled separately anymore.)
+$$((title=Compiling))
 $ mkdir build 
 $ cd build
-$ cmake ..
+$ cmake ../src
 $ make -j3
 $ sudo make install
 $$
-Then compile V3D (the GPU algorithm) which works pretty much the same way:
-$$((title=V3D compiling))
-$ cd V3D
-$ mkdir build 
-$ cd build
-$ cmake ..
-$ make -j3
-$ sudo make install
-$$
-If you want to install slowmoVideo to a different directory (e.g. your home directory) so you don’t need root rights, run cmake as follows instead (both times):
+If you want to install slowmoVideo to a different directory (e.g. your home directory) so you don’t need root rights, run cmake as below instead. By default, slowmoVideo is installed to $$/usr/local/bin/$$.
 $$((title=Different install directory))
 $ cmake .. -DCMAKE_INSTALL_PREFIX=''/home/archibald/slowmoVideo''
 $$
 
-If all of this worked, you should be able to run $$install/bin/slowmoUI$$. If not, please find help in the [https://plus.google.com/u/0/communities/116570263544012246711 Google+ community].
+If all of this worked, you should be able to run $$slowmoUI$$. If not, please find help in the [https://plus.google.com/u/0/communities/116570263544012246711 Google+ community].
